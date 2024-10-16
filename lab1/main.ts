cc9aadb87360ba324a91df89e28059153b86f62d
VeYand <ValerikLerik17@gmail.com>
2024-10-17 01:09:04 +0300

    Добавил примеры

--- lab1/main.ts
+++ lab1/main.ts
@@ -4,13 +4,13 @@ import path from 'node:path'
 const main = () => {
 	const automaton = new Automaton()
 
-	automaton.read(path.join(__dirname, 'samples/mealy/input.txt'), 'Mealy')
+	automaton.read(path.join(__dirname, 'samples/mealy/input1.txt'), 'Mealy')
 	automaton.saveGraph(path.join(__dirname, 'output/mealy1.png'), 'Mealy')
 	automaton.saveGraph(path.join(__dirname, 'output/moore1.png'), 'Moore')
 	automaton.write(path.join(__dirname, 'output/mealy1.txt'), 'Mealy')
 	automaton.write(path.join(__dirname, 'output/moore1.txt'), 'Moore')
 
-	automaton.read(path.join(__dirname, 'samples/moore/input.txt'), 'Moore')
+	automaton.read(path.join(__dirname, 'samples/moore/input1.txt'), 'Moore')
 	automaton.saveGraph(path.join(__dirname, 'output/mealy2.png'), 'Mealy')
 	automaton.saveGraph(path.join(__dirname, 'output/moore2.png'), 'Moore')
 	automaton.write(path.join(__dirname, 'output/mealy2.txt'), 'Mealy')
